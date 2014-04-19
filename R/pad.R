f94c3b0c280f5032dfcbb17118e44dde531bf060
Tyler Rinker <tyler.rinker@gmail.com>
2014-04-19 00:00:00 +0000

    version 1.0.1

--- R/pad.R
+++ R/pad.R
@@ -13,6 +13,12 @@
 #' If character spaces are padded.  The \code{detect} attempts to determine if x 
 #' is numeric (d) or not (s).
 #' @return Returns a character vector every element padded with 0/spaces.
+#' @note \code{pad} is a wrapper for the \code{\link[base]{sprintf}} function.
+#' \code{pad} may behave differently on various platforms in accordance with the
+#' documentation for \code{\link[base]{sprintf}}: "actual implementation will 
+#' follow the C99 standard and fine details (especially the behaviour under user 
+#' error) may depend on the platform."  See \code{\link[base]{sprintf}} for more 
+#' information.
 #' @export
 #' @seealso \code{\link[base]{sprintf}}
 #' @examples
@@ -32,9 +38,29 @@ pad <- function (x, padding = max(nchar(as.character(x))), sort = TRUE,
     if (is.na(type)) {
         type <- ifelse(is.numeric(x), "d", "s")
     }
-    x <- sprintf(paste0("%0", padding, type), x)
+    x <- sprintf_ish(x, padding, type)
     if (sort) {
         x <- sort(x)
     }
     x
 }
+
+sprintf_ish <- function(x, padding, type){
+    OS <- Sys.info()[['sysname']]
+ 
+    if (OS %in% c("Windows", "Linux")) {
+        sprintf(paste0("%0", padding, type), x)
+    } else {
+        type <- ifelse(type == "s", " ", "0")
+        pads <- sapply(padding - nchar(x), function(i)  {
+            if (i == 0) return("")
+            paste(rep(type, i), collapse = "")
+        })
+        paste0(pads, x)
+      
+    }
+}
+
+
+
+
--- tests/testthat/test-lookup.R
+++ tests/testthat/test-lookup.R
@@ -32,13 +32,7 @@ test_that("lookup works with lists",{
 
 })
 
-test_that("lookup is speedy; less than 1.5 sec on 10 mill",{
-    recode_me <- sample(1:(length(LETTERS)*10), 10000000, TRUE)
-    tic <- Sys.time()
-    output <- recode_me %l% split(1:(length(LETTERS)*10), LETTERS)
-    expect_true(as.numeric(difftime(Sys.time(), tic)) < 5)
 
-})
 
 
 
